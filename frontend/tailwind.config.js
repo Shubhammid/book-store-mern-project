49c7891cde42e8a03435989c20ddab232ef12298
Shubham Midgule <shubham.midgule17@gmail.com>
2025-08-23 15:06:49 +0530

    Completed the customization of Projects like Fonts, Colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,8 +4,19 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+   theme: {
+    extend: {
+      colors: {
+        'primary': '#FFCE1A',
+        'secondary' : "#0D0842",
+        'blackBG': '#F3F3F3',
+        'Favorite': '#FF5841'
+      }, 
+      fontFamily: {
+        'primary' : ["Montserrat", "sans-serif"],
+        'secondary' : ["Nunito Sans", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
