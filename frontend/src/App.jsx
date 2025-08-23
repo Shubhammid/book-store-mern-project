cfc9b5c56da7bd85291c0953d4574927e3b2b990
Shubham Midgule <shubham.midgule17@gmail.com>
2025-08-23 15:46:20 +0530

    Added Assests and Created Responsive Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
-      Navbar
+      <Navbar />
       <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
         <Outlet />
       </main>
