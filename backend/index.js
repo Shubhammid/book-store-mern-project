ce55a62e765c81196015f5a25923be6b62da2598
Shubham Midgule <shubham.midgule17@gmail.com>
2025-08-28 12:26:40 +0530

    Completed the Server setup mongoose and mongodb

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose');
+const port =  process.env.PORT ||5000;
+require('dotenv').config()
+
+//shubhammidgule726_db_user
+//1lItEcscqkuvT24Z
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+  app.get('/', (req, res) => {
+  res.send('Hellooooooo World!')
+})
+}
+
+main().then(()=> console.log("MongoDB Connected Successfully")).catch(err => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
