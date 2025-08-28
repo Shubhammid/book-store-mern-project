4ae22aa84a5a7e1f2b52f404f69de64ff30370ab
Shubham Midgule <shubham.midgule17@gmail.com>
2025-08-28 13:36:17 +0530

    Completed Book Routes and Controller and test it on Postman also

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,32 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-const mongoose = require('mongoose');
-const port =  process.env.PORT ||5000;
-require('dotenv').config()
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}))
+
+const bookRoutes = require("./src/books/book.route");
+
+app.use("/api/books", bookRoutes);
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);
-  app.get('/', (req, res) => {
-  res.send('Hellooooooo World!')
-})
+  app.get("/", (req, res) => {
+    res.send("Hellooooooo World!");
+  });
 }
 
-main().then(()=> console.log("MongoDB Connected Successfully")).catch(err => console.log(err));
+main()
+  .then(() => console.log("MongoDB Connected Successfully"))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
